44b6485c0d6caf6286889f20651b225928001859
Bora Manga <bora.mng525@gmail.com>
2020-02-24 21:55:21 +0100

    changed styles to SCSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { Container, Row, Col } from "react-bootstrap";
 import Box from "./components/Box/Box";
 import Description from "./components/Description/Description";
